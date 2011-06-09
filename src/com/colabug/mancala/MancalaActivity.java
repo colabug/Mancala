45e9c04e9d604b59a4bf6ecb6689aa89d1ee700b
Corey Leigh Latislaw <colabug@gmail.com>
2011-06-09 11:53:13 -0400

    Initial commit

---
+++ src/com/colabug/mancala/MancalaActivity.java
@@ -0,0 +1,15 @@
+package com.colabug.mancala;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MancalaActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
