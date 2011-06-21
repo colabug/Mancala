7c7520295a12549e15971ca41afd8872c099e7ca
Corey Leigh Latislaw <colabug@gmail.com>
2011-06-20 21:22:51 -0400

    Added beginnings of the grid adapter .. still not convinced it's the right UI structure to use.
    MancalaGame.java
    * Added grid adapter code, not working right
    
    game_board.xml
    * Added grid view
    
    Board.java
    * Renamed constants & functions
    * Added constant for number rows
    
    Addd images & grid cell xml

--- src/com/colabug/mancala/Board.java
+++ src/com/colabug/mancala/Board.java
@@ -2,7 +2,8 @@ package com.colabug.mancala;
 
 public class Board {
     // Constants
-    private static final int NUMBER_BINS = 8;
+    private static final int NUMBER_COLUMNS = 8;
+    private static final int NUMBER_ROWS = 2;
     private static final int NUMBER_MARBLES = 36;
 
     // Player bins - one for each side of the board
@@ -11,21 +12,21 @@ public class Board {
 
     Board ()
     {
-        player1Bins = new int[NUMBER_BINS];
-        player2Bins = new int[NUMBER_BINS];
+        player1Bins = new int[NUMBER_COLUMNS];
+        player2Bins = new int[NUMBER_COLUMNS];
 
         // Initialize each bin in board with 3 marbles
-        for ( int i = 0; i < NUMBER_BINS; i++ )
+        for ( int i = 1; i < NUMBER_COLUMNS - 1; i++ )
         {
             player1Bins[i] = player2Bins[i] = 3;
         }
 
         // Empty the stores on each side
-        player1Bins[0] = player1Bins[NUMBER_BINS - 1] = 0;
-        player2Bins[0] = player2Bins[NUMBER_BINS - 1] = 0;
+        player1Bins[0] = player1Bins[NUMBER_COLUMNS - 1] = 0;
+        player2Bins[0] = player2Bins[NUMBER_COLUMNS - 1] = 0;
     }
 
-    public String getPlayer1Bins ()
+    public String getPlayer1BinString ()
     {
         String temp = "";
         for ( int bin : player1Bins )
@@ -36,7 +37,7 @@ public class Board {
         return temp;
     }
 
-    public String getPlayer2Bins ()
+    public String getPlayer2BinString ()
     {
         String temp = "";
         for ( int bin : player2Bins )
--- src/com/colabug/mancala/MancalaGame.java
+++ src/com/colabug/mancala/MancalaGame.java
@@ -5,7 +5,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.TextView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.*;
 
 public class MancalaGame extends Activity
 {
@@ -18,6 +21,10 @@ public class MancalaGame extends Activity
 
     // Game data
     private Board board;
+
+    // UI
+    private GridView gridview;
+    private MancalaGameAdapter gridAdapter;
 
     @Override
     public void onCreate (Bundle savedInstanceState)
@@ -37,21 +44,79 @@ public class MancalaGame extends Activity
         TextView welcomeMessage = (TextView) findViewById( R.id.welcome_message );
         String message = getResources().getString( R.string.GAME_WELCOME_TEXT );
         message += " " + player1.getName() + " & " + player2.getName() + "!";
-        welcomeMessage.setText(message);
+        welcomeMessage.setText( message );
 
         // Create game board
         Log.d( TAG, "Creating game board" );
         board = new Board();
 
+        // Set up bin view & adapter
+        gridview = (GridView) findViewById( R.id.gridview );
+        gridAdapter = new MancalaGameAdapter( this );
+        gridview.setAdapter( gridAdapter );
+
+        // TODO: Clicky doesn't work
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id)
+            {
+                Log.d( TAG, "Item clicked at position " + position );
+                v.setBackgroundDrawable( v.getResources().getDrawable( R.drawable.green_marble ) );
+            }
+        });
+
         // Rudimentary display
         TextView player1view = (TextView) findViewById( R.id.player1_bins );
-        player1view.setText( board.getPlayer1Bins() );
+        player1view.setText( board.getPlayer1BinString() );
         TextView player2view = (TextView) findViewById( R.id.player2_bins );
-        player2view.setText( board.getPlayer2Bins() );
+        player2view.setText( board.getPlayer2BinString() );
     }
 
     public static Intent createIntent (Context context)
     {
         return new Intent( context, MancalaGame.class );
+    }
+
+    public class MancalaGameAdapter extends BaseAdapter
+    {
+        private Context context;
+
+        public MancalaGameAdapter (Context c)
+        {
+            context = c;
+        }
+
+        public int getCount()
+        {
+            return 0;
+        }
+
+        public Object getItem (int position)
+        {
+            return null;
+        }
+
+        public long getItemId (int position)
+        {
+            return 0;
+        }
+
+        public View getView (int position, View convertView, ViewGroup parent)
+        {
+            LinearLayout row;
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            // If it's not recycled, initialize attributes
+            if ( convertView == null )
+            {
+                row = new LinearLayout( context );
+            }
+            else
+            {
+                row = (LinearLayout) inflater.inflate( R.layout.mancala_grid_cell, parent, false );
+            }
+
+            return convertView;
+        }
     }
 }
